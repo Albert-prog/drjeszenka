99a40de70e176a57c1e5b6a35276cfeab04ee39d
Albert Szabó Tönki <asztonki@renewtech.co>
2021-08-26 21:44:50 +0200

    smallchangesinthefooter

--- drjeszi/src/components/footer.jsx
+++ drjeszi/src/components/footer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import logo from '../img/logo.png';
+import professions from '../lists/professions';
 
 function Footer() {
   return (
-    <div className="profession">
-      <img src={logo} alt="" className="logoImg2"/>
+    <div className="footerDiv">
+      <div>
+        <img src={logo} alt="" className="logoImg2"/>
+        <p>Valami kamu szöveg</p>
+      </div>
+      <div className="footerFlex">
+        <h3>Navigáció</h3>
+        <ul className="footerList">
+          <a href="#about"><li>Rólam</li></a>
+          <a href="#about"><li>Szakterületek</li></a>
+          <a href="#about"><li>Díjazás</li></a>
+          <a href="#about"><li>Kapcsolat</li></a>
+        </ul>
+      </div>
+      <div className="footerFlex">
+        <h3>Szakterületek</h3>
+        <ul className="footerList">
+          {
+            professions.map((prof, i) => <a href="#professions"><li>{prof.name}</li></a>)
+          }
+        </ul>
+      </div>
     </div>
   )
 }
