771988616344eeb9acb69cd15e38a58c5248f095
Albert Szabó Tönki <asztonki@renewtech.co>
2021-09-16 21:03:08 +0200

    styling

--- drjeszi/src/components/contact.jsx
+++ drjeszi/src/components/contact.jsx
@@ -6,12 +6,15 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Checkbox from '@mui/material/Checkbox';
+import Datapolicy from './Datapolicy';
 
 function Contact() {
 
   const [open, setOpen] = React.useState(false);
   const [scroll, setScroll] = React.useState('paper');
-  const [checked, setChecked] = useState('false');
+  const [checkedData, setCheckedData] = useState('false');
+  const [checkedConsent, setCheckedConsent] = useState('false');
 
   const handleClickOpen = (scrollType) => () => {
     setOpen(true);
@@ -22,7 +25,7 @@ function Contact() {
     setOpen(false);
   };
 
-  const descriptionElementRef = React.useRef(null);
+    const descriptionElementRef = React.useRef(null);
   React.useEffect(() => {
     if (open) {
       const { current: descriptionElement } = descriptionElementRef;
@@ -43,6 +46,7 @@ function Contact() {
       });
       e.target.reset()
   }
+
 
   return (
     <div className="profession" id='contact'>
@@ -68,10 +72,30 @@ function Contact() {
             <div className="col-75">
               <textarea name="message" style={{height:"150px"}} />
             </div>
-            <input type="submit" value="Küldés" className="email" disabled={checked}/>
+            <button name="submit" type="submit" value="Küldés" className="email" disabled={(checkedData||checkedConsent)}>Küldés</button>
             <div className="formConsent">
-            <input type="checkbox" id="consent" defaultChecked={!checked} onChange={()=>setChecked(!checked)}/>
-            <p>Elfogadom az oldal <span onClick={handleClickOpen('paper')}>Adatkezelési tájékoztatóját.</span> és hozzájárulok, hogy felvegyék velem a kapcsolatot.</p>
+              <div>
+                <Checkbox defaultChecked={!checkedData} onChange={()=>setCheckedData(!checkedData)}
+                sx={{
+                  color: '#e9c576',
+                  '&.Mui-checked': {
+                    color: '#e9c576',
+                  },
+                }} 
+                />
+                <p>Egyetértek azzal, hogy az adataimat az oldal elmentse, hogy válaszolni lehessen a megkeresésemre.</p>
+              </div>
+              <div>
+              <Checkbox defaultChecked={!checkedConsent} onChange={()=>setCheckedConsent(!checkedConsent)}
+                sx={{
+                  color: '#e9c576',
+                  '&.Mui-checked': {
+                    color: '#e9c576',
+                  },
+                }} 
+                />
+              <p>Elfogadom az oldal <span onClick={handleClickOpen('paper')}>Adatkezelési tájékoztatóját.</span></p>
+              </div>
             </div>
           </form>
           <Dialog
@@ -88,79 +112,7 @@ function Contact() {
             ref={descriptionElementRef}
             tabIndex={-1}
           >
-                <p>
-                    Tájékoztatjuk, hogy Ön dr. Jeszenka Ádám ügyvéd internet oldalán történő bármely szolgáltatás megrendelésével, használatával és az adatai önkéntes megadásával hozzájárul ahhoz, hogy dr. Jeszenka Ádám ügyvéd az Ön adatait az alább meghatározott célok érdekében a hivatkozott jogszabályi előírások betartása mellett felhasználja.
-                    Tájékoztatjuk továbbá arról, hogy az adatközlés önkéntes, bármikor jogosult az adatkezelésről tájékoztatást kérni, valamint az adatok helyesbítését vagy törlését igényelni dr. Jeszenka Ádám ügyvéd postacímére 1133 Budapest, Váci út 78/A 1. emelet 3. ajtó vagy az ugyved. jeszenka@gmail.com email címre küldött levélben. Az Ön által rendelkezésre bocsátott adatok valódiságáért felelősséget nem vállalunk.
-                    Az Adatkezelő
-                    1. A jelen adatkezelési tájékoztató („ Adatkezelési Szabályzat”) a dr. Jeszenka Ádám ügyvéd (székhelye és postacíme:  1133 Budapest, Váci út 78/A 1. emelet 3. ajtó. adószáma:  56875447-1-41, e-mail címe: ugyved.jeszenka@gmail.com; telefonszáma: ++36703958422) mint adatkezelő („Adatkezelő”) által üzemeltetett, a jelen címen elérhető weboldal („ Weboldal”) felhasználóinak („Felhasználó”) a jelen Adatkezelési Szabályzatban meghatározott személyes adatainak kezelésére vonatkozó adatkezelési szabályait és a Felhasználók tájékoztatóját tartalmazza.
-                    Jogszabályi előírások
-                    2.  A személyes adatok kezelésével kapcsolatos jogszabályi előírásokat Adatkezelő az adatkezelés minden fázisában köteles betartani. Adatkezelő által végzett adatkezelésre elsősorban az alábbi jogszabályokban rögzített rendelkezések az irányadóak:
-                    a Polgári Törvénykönyvről szóló 2013. évi V. törvény 2:43§ (e)
-                    az információs önrendelkezési jogról és az információszabadságról szóló 2011. évi CXII. tv. („Adatvédelmi tv.”);
-                    az elektronikus kereskedelmi szolgáltatások, valamint információs társadalommal összefüggő szolgáltatások egyes kérdéseiről szóló 2001. évi CVIII. tv. („Eker. tv.”);
-                    a gazdasági reklámtevékenység alapvető feltételeiről és egyes korlátairól szóló 2008. évi XLVIII. tv. („Grt. tv.”)
-                    az egyének védelméről a személyes adatok gépi feldolgozása során, Strasbourgban, 1981. január 28. napján kelt Egyezmény kihirdetéséről szóló 1998. évi VI. törvény;
-                    a kutatás és a közvetlen üzletszerzés célját szolgáló név- és lakcímadatok kezeléséről szóló 1995. évi CXIX. Törvény („Katv.”)
-                    
-                    A Felhasználó
-                    3. Felhasználónak minősül minden olyan természetes személy, aki az Adatkezelő bármely szolgáltatását a jelen weboldalon keresztül megrendeli illetve önkéntesen személyes adatokat bocsát az Adatkezelő rendelkezésére, a hírlevélre feliratkozik.
-                    
-                    Az Adatkezelés célja
-                    4. A Felhasználók személyes adatainak kezelésének céljai az alábbiak:
-                    a Weboldal által elérhető szolgáltatások hatékony nyújtása; számlázás
-                    kapcsolattartás, amelynek elsődleges célja a Felhasználók megfelelő tájékoztatása, az esetlegesen felmerülő technikai problémák hatékony és gyors kezelése;
-                    az Weboldalon keresztül leadott megrendelések, jelentkezések nyilvántartása és teljesítése, a megrendelésekkel, képzésekkel, stb. kapcsolatban a Felhasználókkal történő kapcsolattartás;
-                    a  megrendelt szolgáltatások használatával kapcsolatban felmerült viták rendezése,
-                    külön hozzájárulás esetén hírlevél küldése.
-                    
-                    Az adatkezelés jogalapja
-                    5.  Az Adatkezelő a Felhasználó személyes adatait kizárólag a jelen Adatkezelési Szabályzat 4. pontjában meghatározott célok érdekében és a jelen Adatkezelési Szabályzat 7. pontjában meghatározott időtartamig kezeli, valamint biztosítja, hogy az adatkezelés céljának megfelelően történjen. A Felhasználó jelen Adatkezelési Szabályzat elfogadásával kijelenti, hogy az adatkezelésre vonatkozó hozzájárulásának megadása, valamint az ezt követő adatszolgáltatás minden esetben a Felhasználó önkéntes, tájékozott és határozott hozzájárulásán alapul az információs önrendelkezési jogról és az információszabadságról szóló 2011. évi CXII. törvény 5.§.(1) a.) pontja, illetve az elektronikus kereskedelmi szolgáltatások, valamint az információs társadalommal összefüggő szolgáltatások egyes kérdéseiről szóló 2001. évi CVIII. törvény (Eker.tv.) 13/A. bekezdése alapján.
-                    
-                    A kezelt adatok köre
-                    6. A kezelt személyes adatok köre:
-                    a) Felhasználó természetes személyazonosító adatai: vezeték- és utónév;
-                    b) Felhasználó e-mail címe
-                    
-                    Adatkezelés időtartalma
-                    7. A Felhasználó hozzájárulása alapján kezelt adatok a hozzájárulás módosításáig, illetőleg visszavonásáig kezelhetők. Az adatkezelés időtartamának lejártával a Felhasználó személyes adatait az Adatkezelő törölni köteles.
-                    8. Az adatkezelés időtartama a Szerződés megszűnésétől számított öt év, tekintettel arra, hogy a Szerződés megszűnését követően ennyi időn belül merülhet fel az Adatkezelőnek, harmadik személynek a Felhasználóval vagy a Felhasználó tevékenysége miatt az Adatkezelővel szembeni polgári jogi igénye, és így biztosítja, hogy a Felhasználó kiléte visszakereshető maradjon.
-                    Az Adatkezelő törvényi kötelezettségének eleget téve a könyvviteli elszámolást közvetlenül és közvetetten alátámasztó számviteli bizonylatot (ideértve a főkönyvi számlákat, az analitikus, illetve részletező nyilvántartásokat is), legalább 8 évig megőrzi.
-                    
-                    Adatfeldolgozó
-                    9. Adatfeldolgozó az a természetes vagy jogi személy, illetve jogi személyiséggel nem rendelkező szervezet, aki vagy amely szerződés alapján – beleértve a jogszabály rendelkezése alapján kötött szerződést is – adatok feldolgozását végzi.
-                    
-                    Adatok biztonsága
-                    10. A kezelt adatokhoz kizárólag dr. Jeszenka ügyvéd férhet hozzá.
-                    11. Az Adatkezelő és adatfeldolgozók az Info törvény 7. §-nak megfelelően intézkednek a kezelt adatok biztonságáról: az adatokat elzárva, jelszóval és fizikai úton védett szerveren tárolja, lehetetlenné téve illetéktelen személyeknek az adatokhoz való hozzáférést.
-                    12. A Felhasználó elismeri és elfogadja, hogy személyes adatainak a weboldalon való megadása esetén – annak ellenére, hogy az Adatkezelő az adatokhoz való jogosulatlan hozzáférésnek vagy azok kifürkészésének megelőzésére korszerű biztonsági eszközökkel rendelkezik –, az adatok védelme teljes mértékben az Interneten nem garantálható. Jogosulatlan hozzáférés vagy adatmegismerés erőfeszítéseink ellenére történő bekövetkezése esetén az Adatkezelő nem felelős ilyen jellegű adatmegszerzésért vagy jogosulatlan hozzáférésért vagy a Felhasználónál ezen okokból kifolyólag keletkezett bármilyen kárért. Ezen felül a Felhasználó is megadhatja személyes adatait harmadik személyeknek, akik azt jogellenes célra vagy módon is felhasználhatják.
-                    13. A Felhasználó nem járul hozzá személyes adatai nyilvánossá tételéhez.
-                    A felhasználó jogai
-                    14. Az adatkezelő az érintett kérelmére 10 napon belül közérthető formában, az érintett erre irányuló kérelmére írásban- tájékoztatást ad az érintett részére a kezelt és az érintettre vonatkozó személyes adatokról, azok forrásáról, az adatkezelés céljáról, jogalapjáról, időtartamáról, az adatfeldolgozó nevéről, címéről és az adatkezeléssel összefüggő tevékenységéről, adatvédelmi incidens esetén az adatvédelmi incidens körülményeiről, hatásairól és az elhárítására megtett intézkedésekről, továbbá – az érintett személyes adatainak továbbítása esetén – az adattovábbítás jogalapjáról és címzettjéről. A tájékoztatás ingyenes.
-                    15. Az érintett kérheti személyes adatainak helyesbítését, továbbá – törvény vagy – törvény felhatalmazása alapján, az abban meghatározott körben – helyi önkormányzat rendelete közérdeken alapuló célból elrendelt adatkezelések kivételével– adatainak a zárolását vagy törlését.
-                    16. Az érintett  bármikor leiratkozhat a hírlevélről és ez esetben Adatkezelő azonnal köteles törölni a kezelt adatokat. Leiratkozás az 1. pontban részletesen kifejtett postacímen és e-mail címen.
-                    17. Az érintett az Info tv-ben meghatározott esetekben tiltakozhat személyes adatainak a kezelése ellen. A tiltakozást Adatkezelő annak benyújtásától számított legrövidebb időn belül, de legfeljebb 15 napon belül megvizsgálja, annak megalapozottsága kérdésében döntést hoz, és döntéséről az érintettet írásban tájékoztatja. Amennyiben az érintett a döntéssel nem ért egyet, illetve az Adatkezelő a fenti határidőt elmulasztja, úgy a döntés közlésétől, illetve a határidő utolsó napjától számított 30 napon belül jogosult bírósághoz fordulni.
-                    18. Az érintettek a személyes adataik kezelésével kapcsolatos tiltakozásaikat, kérelmeiket társaságunknál szóban (személyesen) vagy írásban jelenthetik be az 1. pontban részletesen kifejtett postacímen és e-mail címen.
-                    19. Amennyiben Önnek a személyes adataival kapcsolatos tiltakozását, panaszát, kérelmeit Adatkezelőnél nem sikerült megnyugtató módon rendeznie, vagy Ön bármikor úgy ítéli meg, hogy személyes adatai kezelésével kapcsolatban jogsérelem következett be, vagy annak közvetlen veszélye fennáll, úgy a Nemzeti Adatvédelmi és Információszabadság Hatóságnál jogosult bejelentést tenni. A Nemzeti
-                    Adatvédelmi és Információszabadság Hatóság elérhetőségei :
-                    Székhely: 1125 Budapest, Szilágyi Erzsébet fasor 22/c.
-                    Levelezési cím: 1530 Budapest, Pf.: 5.
-                    Telefon: (+36 1) 391-1400 Telefax: (+36 1) 391-1410
-                    E-mail: ugyfelszolgalat@naih.hu
-                    Web: www.naih.hu
-                    
-                    20. Személyes adatainak a védelméhez fűződő jogainak a megsértése esetén Ön jogosult bírósághoz fordulni.  A per elbírálása a törvényszék hatáskörébe tartozik.
-                    A per – az Ön választása szerint – az Ön lakóhelye vagy tartózkodási helye szerinti törvényszék előtt is megindítható.
-                    21. Az Weboldal harmadik személyek által üzemeltetett weboldalakra mutató linkeket is tartalmazhat. Az ilyen weboldalakon található tartalom nem áll az Adatkezelő befolyása alatt, így azokért semminemű felelősséget nem vállal.
-                    22. Amennyiben az adatkezelési szabályzatot nem áll módjában teljes körűen elfogadni, vagy azzal kapcsolatban bármilyen fenntartással él, kérjük, hogy a szolgáltatásokat ne rendelje meg, adatokat ne bocsásson Adatkezelő rendelkezésére.
-                    Cookie
-                    22. Az Adatkezelő a Felhasználó számítógépére anonim igénybe vevő azonosítót (cookie, süti) helyez el, amely önmagában semmilyen módon nem képes a Felhasználó azonosítására, kizárólag a Felhasználó gépének felismerésére alkalmas, név, e-mail cím vagy bármilyen más személyes információ megadása nem szükséges, mivel a megoldás alkalmazásakor a Felhasználó az Adatkezelőnek nem ad át személyes adatot, az adatcsere csak és kizárólag a gépek között történik meg.
-                    Adatkezelő a cookie-kat abból a célból kezeli, hogy többet tudhasson meg a Felhasználó információhasználati szokásairól és így, javíthassa szolgáltatásai színvonalát, valamint a weboldal látogatása során testreszabott oldalakat, marketing (reklám) anyagokat jelentessen meg.
-                    A Felhasználónak lehetősége van arra, hogy böngészője beállításával megtiltsa az egyedei azonosító jel (cookie) elhelyezését a számítógépén. Az Felhasználó tudomásul veszi, hogy a cookie-k letiltása esetén egyes szolgáltatások nem fognak megfelelően működni.
-                    
-                          dr. Jeszenka Ádám ügyvéd
-                          Adatkezelő
-
-                </p>
+                <Datapolicy/>
             </DialogContentText>
             </DialogContent>
             <DialogActions>
